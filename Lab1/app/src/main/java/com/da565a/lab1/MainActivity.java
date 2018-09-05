8d48802f1a9a9b664018881d797a9ba4760fbeb6
Penguin <cool@ias.org>
2018-09-06 00:16:38 +0200

    initial aide comet
    don't know if all this s*** is needed

---
+++ Lab1/app/src/main/java/com/da565a/lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.da565a.lab1;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
